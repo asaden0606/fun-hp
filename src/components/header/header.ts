dfe3677df72afe3b7dae4665c2c7fe9ecef1c3c1
asaden0606 <asaden0606@gmail.com>
2021-02-23 11:01:27 +0900

    タイトルの実装

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,5 +1,5 @@
 import { Component, Prop, Vue, Provide } from "vue-property-decorator";
-
+import $ from 'jquery';
 const TITLE_PNG = require('@/assets/title.png');
 
 @Component({
@@ -10,5 +10,30 @@ export default class App extends Vue {
     TITLE_PNG = TITLE_PNG;
     created() {
 
+    }
+
+    mounted(){
+        this.createRupan();    
+    }
+
+
+    private getRandom(min: number, max: number): number {
+        return Math.random() * (max - min) + min;
+    }
+
+    private createRupan() {
+        let $title = $('#title');
+        let titleText = $title.text();
+        $title.show();
+        $title.text("");
+        console.log(titleText);
+
+        for (let i = 0; i < titleText.length; i++) {
+            let c = titleText.substring(i, i + 1);
+            let $item = $(`<text>${c}</text>`).appendTo($title);
+            $item.css('left',this.getRandom(-500, 500));
+            $item.css('top', this.getRandom(-500, 500));
+            $item.animate({ left: 0, top: 0 }, 2000);
+        }
     }
 }
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -2,14 +2,32 @@
 </script>
 <template>
   <div>
-    <div class='header'>
-      <img :src="TITLE_PNG" height="150px" />
-      <div class="title">あさでんのサイト</div>
+    <div class="header">
+      <img :src="TITLE_PNG" width='100%' height='150px' />
+      <div id="title" class="title">あさでんのサイト</div>
     </div>
   </div>
 </template>
 <style lang='scss'>
-    .header{
-        text-align: center;
-    }
+.header {
+  text-align: center;
+}
+
+.title {
+  text-align: center;
+  position: relative;
+  top: -80px;
+  margin-bottom: -90px;
+}
+
+.title text {
+  color: #dddddd;
+  text-shadow: 5px 5px 1px #111111;
+  font-size: 400%;
+  position: relative;
+}
+
+.title {
+  display: none;
+}
 </style>
