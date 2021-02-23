f675b14b8dabfed101fa2a07974eb9492fdb6fc1
asaden0606 <asaden0606@gmail.com>
2021-02-23 09:53:38 +0900

    ポート番号の変更

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  publicPath: '/',
+  lintOnSave: false,
+    devServer: {
+        port: 3001,
+        disableHostCheck: true,
+        proxy: {
+          "/api/": {
+          	target: "http://funbarisoft.dip.jp",            
+            //target: "http://localhost:8080",
+          }       
+        }
+    },
+    
+    configureWebpack: {
+        resolve: {
+          alias: {
+            "d3":"d3/build/d3.min.js",
+            "techan": "techan/dist/techan.min.js",
+          }
+        }
+    }
+};
