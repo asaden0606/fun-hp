8907eaabb9ba28f4230d199ea3f40bb2016e2ef8
asaden0606 <asaden0606@gmail.com>
2021-02-27 08:56:24 +0900

    headerの修正

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,35 +1,44 @@
 <script lang='ts' src='./header.ts'>
 </script>
 <template>
-    <div class='header'>
-      <img src="@/assets/title.png" width='80%' height='150px' />
-      <div id="title" class="title">あさでんのサイト</div>
-    </div>
+  <div class="header">
+    <img class="bk" src="@/assets/title.png" />
+    <div id="title" class="title">あさでんのサイト</div>
+  </div>
 </template>
-<style lang='scss'>
+<style lang='scss' >
 @import "@/css/share";
 .header {
-  text-align: center;  
+  text-align: center;
+}
+
+.bk {
+  width: 80%;
+  height: 150px;
+
+  @include sp($sph){
+    width:100%;  
+  }
 }
 
 .title {
   text-align: center;
   position: relative;
   top: -50px;
-  margin-bottom:-60px;
+  margin-bottom: -60px;
 
-  @include sp($sph){
-    //top:-100px;  
+  @include sp($sph) {
+    //top:-100px;
   }
 }
 
 .title text {
   color: #dddddd;
   text-shadow: 5px 5px 1px #111111;
-  font-size:5rem;
+  font-size: 5rem;
   position: relative;
-  @include sp($sph){
-    font-size:3rem;
+  @include sp($sph) {
+    font-size: 3rem;
   }
 }
 
