216b377d7449ae430991469041f8c6bef4f0bc18
Takahiro Yamauchi <asaden0606@gmail.com>
2023-05-18 11:24:09 +0900

    vue-final-modalへの対応

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { install } from 'fun-chart';
+import Chart from 'fun-chart';
 import "fun-chart/dist/fun-chart.css";
-import { createVfm } from 'vue-final-modal'
-const vfm = createVfm()
 
-createApp(App).use(vfm).use(router)
-    .use(install).mount('#app')
+createApp(App).use(router)
+    .use(Chart).mount('#app')
